16f34cd62faa8debc9b38ce022825046339e9f5b
nadezhda <nadezhda.bessonova@gmail.com>
2024-05-08 15:23:02 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/FemilyMember.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Program;
+
+namespace ConsoleApp1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    internal class FemilyMember
+    {
+        public FemilyMember Mother { get; set; }
+        public FemilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime Bird;
+     
+        public class AdultFamilyMember : FemilyMember
+        {
+            public AdultFamilyMember[] Children { get; set; }
+        } 
+        public void ShowParents() //вывод родителей
+        {
+            Console.WriteLine($"мать: {Mother?.Name}, отец: {Father?.Name}");
+        }
+
+        public void ShowFemeliInfo() //вывод дерева
+        {
+            Console.WriteLine($"бабушка: {Mother?.Mother?.Name}, дедушка: {Father?.Mother?.Name}, \n" +
+                $"бабушка: {Mother?.Father?.Name}, дедушка: {Father?.Father?.Name}, \n" +
+                $"мать: {Mother?.Name}, отец: {Father?.Name}, \n " +
+                $"имя: {Name}, пол: {Gender}, дата рождения: {Bird}");
+        }
+
+        public void PersonInfo() //информация о человеке
+        {
+            Console.WriteLine($"имя: {Name}, пол: {Gender}, дата рождения: {Bird}");
+        }
+
+        public void PrintFamilyTree() // вывод детей всех поколений
+        { 
+            foreach(var child in Children)
+            {
+                if (child.Cildren is not null)
+                {
+                    PersonInfo();
+                    PrintFamilyTree();
+                }
+            }
+        }
+    }  
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+using static ConsoleApp1.Program;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {         
+        static void Main(string[] args)
+        {
+            
+            var grandfather = new FemilyMember.AdultFamilyMember() { Mother = null, Father = null, Name = "Иван", Gender = Gender.Male, Bird = DateTime.Parse("01.09.1950") };
+            var grandmother = new FemilyMember.AdultFamilyMember() { Mother = null, Father = null, Name = "Мария", Gender = Gender.Female, Bird = DateTime.Parse("20.05.1950") };
+            var fater = new FemilyMember.AdultFamilyMember() { Mother = grandfather, Father = null, Name = "Михаил", Gender = Gender.Male, Bird = DateTime.Parse("08.09.1970") };
+            var muter = new FemilyMember.AdultFamilyMember() { Mother = null, Father = grandmother, Name = "Ольга", Gender = Gender.Female, Bird = DateTime.Parse("01.10.1962") };
+            var son = new FemilyMember() { Mother = muter, Father = fater, Name = "Николай", Gender = Gender.Male, Bird = DateTime.Parse("13.04.1998") };
+            var daughter = new FemilyMember() { Mother = muter, Father = fater, Name = "Анастасия", Gender = Gender.Female, Bird = DateTime.Parse("18.12.2000")};
+            
+            muter.Children = new FemilyMember.AdultFemilyMember[] { son, daughter };
+            fater.Children = new FemilyMember.AdultFamilyMember[] { son, daughter };
+            grandfather.Children = FemilyMember.AdultFamilyMember[] {  fater };
+            grandmother.Children = FemilyMember.AdultFamilyMember[] {  muter };
+
+
+
+
+            son.ShowParents();
+            Console.WriteLine();
+            Console.WriteLine();
+            daughter.ShowParents();
+            Console.WriteLine();
+            Console.WriteLine();
+            son.ShowFemeliInfo();
+            Console.WriteLine();
+            Console.WriteLine();
+            
+        }
+    }
+}
